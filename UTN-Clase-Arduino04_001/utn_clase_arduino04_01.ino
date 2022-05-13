f3bd971fad42c18b29ea5f24f1d37cda497eb39b
LucasLodigiani <103153491+LucasLodigiani@users.noreply.github.com>
2022-05-13 22:54:05 +0200

    Add files via upload

---
+++ UTN-Clase-Arduino04_001/utn_clase_arduino04_01.ino
@@ -0,0 +1,26 @@
+int Led_Pin = 3;
+int Fot_Pin = A0;
+int sensorValue = 0;
+int outputValue = 0;
+void setup()
+{
+Serial.begin(9600);
+pinMode(Led_Pin, OUTPUT);
+}
+void loop()
+{
+
+ digitalWrite(Led_Pin, HIGH);
+
+ sensorValue = analogRead(Fot_Pin);
+ outputValue = map(sensorValue, 0, 1023, 0, 255);
+ analogWrite(Led_Pin, outputValue);
+ delay(2000);
+ Serial.print("Lectura: ");
+ Serial.println(sensorValue);
+ Serial.print("Mapeado: ");
+ Serial.println(outputValue);
+ delay(250);
+ digitalWrite(Led_Pin, LOW);
+ delay(250);
+}
